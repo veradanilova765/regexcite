9a670b4f6f731992df68c4377cb03528a4427407
Vera Danilova <veradanilova765@gmail.com>
2023-04-03 16:55:47 -0700

    added a space

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -13,7 +13,8 @@
 #'
 #' y <- "192.168.0.1"
 #' str_split_one(y, pattern = stringr::fixed("."))
-str_split_one <- function(string, pattern, n = Inf) {
+
+  str_split_one <- function(string, pattern, n = Inf) {
   stopifnot(is.character(string), length(string) <= 1)
   if (length(string) == 1) {
     stringr::str_split(string = string, pattern = pattern, n = n)[[1]]
